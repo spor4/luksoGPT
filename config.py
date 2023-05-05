1008d984fa086dcc84e55225ea9f350e5272f7f9
spor4 <88740309+spor4@users.noreply.github.com>
2023-05-05 19:03:37 +0200

    luksoGPT
    luksoGPT

---
+++ config.py
@@ -0,0 +1,3 @@
+# .gitignore 
+github_api_key = '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'
+openai_api_key = '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'
---
+++ github_api_wrapper.py
@@ -0,0 +1,25 @@
+# github_api_wrapper.py
+
+import requests
+
+class GitHubAPIWrapper:
+    def __init__(self, api_token: str):
+        self.api_token = api_token
+        self.base_url = "https://api.github.com"
+
+    def _make_request(self, url: str) -> dict:
+        headers = {"Authorization": f"token {self.api_token}"}
+        response = requests.get(url, headers=headers)
+
+        if response.status_code != 200:
+            raise Exception(f"GitHub API request failed: {response.text}")
+
+        return response.json()
+
+    def get_repo_info(self, owner: str, repo: str, subdir: str = None) -> dict:
+        if subdir:
+            url = f"{self.base_url}/repos/{owner}/{repo}/contents/{subdir}"
+        else:
+            url = f"{self.base_url}/repos/{owner}/{repo}"
+
+        return self._make_request(url)
